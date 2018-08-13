228500804887cc9f4997aba6bb00cb6915907f6b
Christoph Lembeck <lec@viadee.de>
2018-08-13 19:40:23 +0200

    renamed

---
+++ jremotehtml-core/src/main/java/de/chrlembeck/jremotehtml/core/element/HTMLDomNode.java
@@ -0,0 +1,27 @@
+package de.chrlembeck.jremotehtml.core.element;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.Writer;
+import java.util.List;
+
+import de.chrlembeck.jremotehtml.core.change.Change;
+
+public interface HTMLElement extends Serializable {
+
+    void render(Writer writer) throws IOException;
+
+    void collectListeners(List<Change> listenerChanges);
+
+    void collectStyles(List<Change> styleChanges);
+
+    /**
+     * Prüft, ob der Knoten dem Client schon bekannt ist, also bereits einmal an
+     * den Client gesendet wurde.
+     */
+    boolean isNewNode();
+
+    void unsetIds();
+
+    void setParent(Tag tag);
+}
